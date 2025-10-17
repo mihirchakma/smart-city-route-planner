5521d8a72ca0a58b62fa07ec3e2e9af33b94cec6
“[firstname <ruchiravishvajith6@gmail.com>
2025-10-17 12:48:41 +0530

    Manage the core logic of the route planner

--- SmartCityRoutePlanner/src/com/smartcity/service/RoutePlanner.java
+++ SmartCityRoutePlanner/src/com/smartcity/service/RoutePlanner.java
@@ -1,6 +1,16 @@
 package com.smartcity.service;
 
+import com.smartcity.model.CityGraph;
+
+import com.smartcity.model.LocationAVLTree;
+
 // (Member 2: Management Logic)
 
 public class RoutePlanner {
+    private final LocationAVLTree locationTree;
+    private final CityGraph cityMap;
+
+    public RoutePlanner() {
+        this.locationTree = new LocationAVLTree();
+        this.cityMap = new CityGraph();
 }
