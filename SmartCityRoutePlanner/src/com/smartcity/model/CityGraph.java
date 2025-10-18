9ee9b7e2a27bbe47cecf354a849c6b805a1efe4f
SaleelaKaushal <saleelakaushal@gmail.com>
2025-10-18 10:44:58 +0530

    Update CityGraph.java
    Add BFS traversal method for the city graph

--- SmartCityRoutePlanner/src/com/smartcity/model/CityGraph.java
+++ SmartCityRoutePlanner/src/com/smartcity/model/CityGraph.java
@@ -64,17 +64,61 @@ public void removeLocation(String location)
     }
 }
 
-public void displayConnections() {
+public void displayConnections() 
+{
     System.out.println("\n--- City Road Connections ---");
-    if (adjList.isEmpty()) {
+
+    if (adjList.isEmpty())
+     {
         System.out.println("No connections to display.");
-    } else {
+    } 
+
+    else 
+    {
         for (Map.Entry<String, List<String>> entry : adjList.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
     }
     System.out.println("---------------------------\n");
+
 }
+
+
+public void performBfs(String startNode)
+ {
+    if (!adjList.containsKey(startNode)) 
+    {
+        System.out.println("Start location for traversal not found.");
+        return;
+    }
+
+    System.out.println("--- Breadth-First Traversal starting from " + startNode + " ---");
+    Queue<String> queue = new LinkedList<>();
+    Set<String> visited = new HashSet<>();
+
+    queue.add(startNode);
+    visited.add(startNode);
+
+    while (!queue.isEmpty()) 
+    {
+        String currentLocation = queue.poll();
+        System.out.print(currentLocation + " ");
+
+        for (String neighbor : adjList.get(currentLocation))
+         {
+            if (!visited.contains(neighbor)) 
+            
+            {
+                visited.add(neighbor);
+                queue.add(neighbor);
+            }
+        }
+    }
+
+    System.out.println("\n----------------------------------------------------");
+}
+
+
 
 
 
